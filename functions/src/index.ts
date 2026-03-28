f893a1fc05d1bead2a87a1cf92c4dca969d16e72
Chetan Padia <chetbox@users.noreply.github.com>
2026-03-28 21:47:32 +0000

    move `admin.database()` into functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,14 +1,12 @@
-import * as admin from 'firebase-admin';
+import { database, initializeApp } from 'firebase-admin';
 import { onCall, HttpsError } from 'firebase-functions/v2/https';
 import { onValueWritten } from 'firebase-functions/v2/database';
 
-admin.initializeApp({
+initializeApp({
   databaseAuthVariableOverride: {
     uid: 'cloudfunction@kaleidoscope'
   }
 });
-
-const database = admin.database();
 
 interface Connection {
   peerId: String;
@@ -22,7 +20,7 @@ export const requestConnectionV2 = onCall(async (request) => {
     throw new HttpsError('unauthenticated', 'User must be authenticated');
   }
   const myUid = request.auth.uid;
-  const connections = (await database.ref('/connection').once('value')).val() || {} as Connections;
+  const connections: Connections = (await database().ref('/connection').once('value')).val() || {};
 
   const uidsOnline = Object.keys(connections).filter(uid => uid !== myUid);
   const uidsWaiting = uidsOnline.filter(uid => !connections[uid].watching);
@@ -34,12 +32,12 @@ export const requestConnectionV2 = onCall(async (request) => {
   const randomWaitingUid = uidsWaiting[Math.floor(Math.random() * uidsWaiting.length)];
   console.log('selected', randomWaitingUid);
 
-  await database.ref('/connection').child(randomWaitingUid).child('watching').set(myUid);
+  await database().ref('/connection').child(randomWaitingUid).child('watching').set(myUid);
   return randomWaitingUid;
 });
 
 export const countConnectionsV2 = onValueWritten('/connection', async (event) => {
   const connections = event.data.after.val() || {} as Connections;
   const count = Object.keys(connections).length;
-  await database.ref('/stats').child('connections').child('count').set(count);
+  await database().ref('/stats').child('connections').child('count').set(count);
 });
